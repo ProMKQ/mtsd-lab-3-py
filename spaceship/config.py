6ad225f1c004aad77e872ba434db366da8c48dae
ProMKQ <promkq@gmail.com>
2025-06-08 19:07:34 +0300

    Minor changes

--- spaceship/config.py
+++ spaceship/config.py
@@ -5,6 +5,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'spaceship'
-    app_description: str = 'Laboratory assignment 03 starter project for software engineering course'
+    app_description: str = 'Laboratory assignment #3 starter project for software engineering course'
     app_version: str = 'dev'
     debug: bool = False
